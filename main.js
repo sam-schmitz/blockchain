733652974c316d7a9a9bc08f88c2556953c68420
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-07-17 20:35:01 -0500

    Signatures are now working properly. Had to fix error with generateKeyPair being an async function

--- main.js
+++ main.js
@@ -4,7 +4,11 @@
 const SHA256 = require('crypto-js/sha256');
 const SHA3 = require('crypto-js/sha3');
 //var hash = SHA3("Message", {outputLength: 224});
-const { generateKeyPair, createCipheriv } = require('crypto');
+const { generateKeyPair, createCipheriv, privateEncrypt, createSign } = require('crypto');
+const util = require('util');
+const { promisify } = require('util');
+const generateKeyPairPromise = util.promisify(generateKeyPair);
+const crypto = require('crypto');
 
 class Block {
 	constructor(index, timestamp, data, previousHash) {
@@ -72,73 +76,96 @@ class Blockchain{
 	}
 }
 
+const generateKeyPair2 = promisify(crypto.generateKeyPair);
+
 class Wallet {
-	constructor(pk) {
-		//generate asynchronous key pair
-		/*generateKeyPair('rsa', {
-			modulousLength: 530, // Options
-			publicKeyEncoding: {
-				type: 'pkcs1',
-				format: 'der'
-			},
-			privateKeyEncoding: {
-				type: 'pkcs1',
-				format: 'der',
-				//cipher: 'aes-192-cbc',
-				//passphrase: 'Welcome'
-			}
-		}, (err, publicKey, privateKey) => { // Callback function
-			if (!err) {
-				// This will print the asymmetric key pair
-				this.publicKey = publicKey
-				this.privateKey = privateKey
-			} else {
-				// Prints error if any
-				console.log("Errr is: ", err);
-			}
-		});
-		*/
-		this.publicKey = pk
+	constructor() {
+		this.publicKey = null;
+		this.privateKey = null;
+		this.init();
+	}
+
+	async init() {
+		try {
+			//generate key pair
+			const { publicKey, privateKey } = await generateKeyPair2('rsa', {
+				modulusLength: 2048,
+				publicKeyEncoding: {
+					type: 'spki',
+					format: 'pem',
+				},
+				privateKeyEncoding: {
+					type: 'pkcs8',
+					format: 'pem',
+				},
+			});
+
+			this.publicKey = publicKey;
+			this.privateKey = privateKey;
+		} catch (error) {
+			console.error('Error generating keys:', error);
+		}
 	}
 
 	createTransaction(amount, recipient) {
 		//create a string with the transaction data
-		let transaction = `${this.publicKey} pays ${recipient} ${amount}`;
+		let transaction = `${this.publicKey.toString()} pays ${recipient.toString()} ${amount}`;
 
 		//generate a signature
-		//let signature = this.generateSignature(transaction);
+		let signature = this.generateSignature(transaction);
 
 		//return a dictionary with the message and signature
-		//return { data: transaction, signature: signature };
-		return transaction
+		return { data: transaction, signature: signature };
+		//return transaction
 	}
 
 	generateSignature(data) {
-		//run the transaction data through a hash
-		let hash = SHA256(data);
+		//verify that the private key has been created
+		if (!this.privateKey) {
+			throw new Error('Public key not generated yet');
+		}
 
-		//encrpyt the hash w/ private key -> signature
-		return createCipheriv('rsa', this.privateKey, hash);
-    }
+		//run the transaction data through a hash
+		const hash = crypto.createHash('sha256').update(data).digest();
+
+
+		//encrypt the hash using the private key
+		const encryptedHash = crypto.privateEncrypt(this.privateKey, Buffer.from(hash));
+		return encryptedHash.toString('base64');
+	}
+
+	encryptMessage(message) {
+		if (!this.publicKey) {
+			throw new Error('Public key not generated yet');
+		}
+
+		const encryptedMessage = crypto.publicEncrypt(this.publicKey, Buffer.from(message));
+		return encryptedMessage.toString('base64')
+	}
 }
 
 let jsChain = new Blockchain();
 
-console.log("creating wallets...");
-let wallet1 = new Wallet("1");
-let wallet2 = new Wallet("2");
+(async () => {
+	console.log("creating wallets...");
+	const wallet1 = new Wallet();
+	const wallet2 = new Wallet();
 
-console.log("creating transactions...");
-let transaction1 = wallet1.createTransaction("5", wallet2.publicKey);
-let transaction2 = wallet2.createTransaction("10", wallet1.publicKey);
+	await new Promise(resolve => setTimeout(resolve, 1000));
 
-console.log("mining in process");
-jsChain.addBlock(
-	new Block(1, "12/25/2024", transaction1, jsChain.latestBlock().hash)
-);
-jsChain.addBlock(
-	new Block(2, "12/26/2024", transaction2, jsChain.latestBlock().hash)
-);
+	console.log("creating transactions...");
+	let transaction1 = wallet1.createTransaction("5", wallet2.publicKey);
+	let transaction2 = wallet2.createTransaction("10", wallet1.publicKey);
 
-console.log(JSON.stringify(jsChain, null, 4));
-console.log("Is blockchain valid? " + jsChain.checkValid());
+	console.log("mining in progress...");
+	jsChain.addBlock(
+		new Block(1, "12/25/2024", transaction1, jsChain.latestBlock().hash)
+	);
+	jsChain.addBlock(
+		new Block(2, "12/26/2024", transaction2, jsChain.latestBlock().hash)
+	);
+
+
+	console.log(JSON.stringify(jsChain, null, 4));
+	console.log("Is blockchain valid? " + jsChain.checkValid());
+})();
